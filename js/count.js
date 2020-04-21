781e76ed11109f6be3879accfbc492b39c429e90
Leonid2123 <60269702+Leonid2123@users.noreply.github.com>
2020-04-21 17:54:32 +0300

    Update count.js

--- js/count.js
+++ js/count.js
@@ -19,3 +19,117 @@ $(document).ready(function(){
 
  
 });
+<script type="text/javascript">
+		$('.work').hide();
+		$('.wimg1').click(function(){
+			$('.w1').fadeIn(200);
+		});
+		$('.wimg2').click(function(){
+			$('.w2').fadeIn(200);
+		});
+		$('.wimg3').click(function(){
+			$('.w3').fadeIn(200);
+		});
+		$('.wimg4').click(function(){
+			$('.w4').fadeIn(200);
+		});
+		$('.wimg5').click(function(){
+			$('.w5').fadeIn(200);
+		});
+		$('.wimg6').click(function(){
+			$('.w6').fadeIn(200);
+		});
+		$('.w1').click(function(){
+			$('.w1').fadeOut(200);
+		});
+		$('.w2').click(function(){
+			$('.w2').fadeOut(200);
+		});
+		$('.w3').click(function(){
+			$('.w3').fadeOut(200);
+		});
+		$('.w4').click(function(){
+			$('.w4').fadeOut(200);
+		});
+		$('.w5').click(function(){
+			$('.w5').fadeOut(200);
+		});
+		$('.w6').click(function(){
+			$('.w6').fadeOut(200);
+		});
+
+
+		$('.open').hide();
+		$('.setimage').click(function(){
+			$('.open').fadeToggle(200);
+		});
+		$('.workline').not(':first').hide();
+		$('.btn1').click(function(){
+			$('.wl1').fadeToggle(0);
+			$('.wl2').fadeToggle(0);
+		});
+		$('.btn2').click(function(){
+			$('.wl1').fadeToggle(0);
+			$('.wl2').fadeToggle(0);
+		});
+		$('.article').not(':first').hide();
+		$('.linetext').not(':first').hide();
+		$('#but1').click(function(){
+			$('#lt1').fadeIn(200);
+			$('#lt2').fadeOut(200);
+			$('#lt3').fadeOut(200);
+			$('#lt4').fadeOut(200);
+			$('.artabout').fadeIn(200);
+			$('.artskills').fadeOut(0);
+			$('.artworks').fadeOut(0);
+			$('.artcontacts').fadeOut(0);
+		});
+		$('#but2').click(function(){
+			$('#lt1').fadeOut(200);
+			$('#lt2').fadeIn(200);
+			$('#lt3').fadeOut(200);
+			$('#lt4').fadeOut(200);
+			$('.artabout').fadeOut(0);
+			$('.artskills').fadeIn(200);
+			$('.artworks').fadeOut(0);
+			$('.artcontacts').fadeOut(0);
+		});
+		$('#but3').click(function(){
+			$('#lt1').fadeOut(200);
+			$('#lt2').fadeOut(200);
+			$('#lt3').fadeIn(200);
+			$('#lt4').fadeOut(200);
+			$('.artabout').fadeOut(0);
+			$('.artskills').fadeOut(0);
+			$('.artworks').fadeIn(200);
+			$('.artcontacts').fadeOut(0);
+		});
+		$('#but4').click(function(){
+			$('#lt1').fadeOut(200);
+			$('#lt2').fadeOut(200);
+			$('#lt3').fadeOut(200);
+			$('#lt4').fadeIn(200);
+			$('.artabout').fadeOut(0);
+			$('.artskills').fadeOut(0);
+			$('.artworks').fadeOut(0);
+			$('.artcontacts').fadeIn(200);
+		});
+		$('.color1').click(function(){
+			$('body').attr('style', 'background: #18b0b0;');
+			$('.left_side').attr('style', 'background: #18b0b0;');
+			$('.button').attr('style', 'background: #de7119;');
+			$('.niceline').attr('style', 'background: #de7119; box-shadow:-2px -0px rgb(196, 101, 25), -1px -1px rgb(237,238,239),-3px -1px rgb(196, 101, 25), -2px -2px rgb(237,238,239),-4px -2px rgb(196, 101, 25), -3px -3px rgb(237,238,239),-5px -3px rgb(196, 101, 25), -4px -4px rgb(237,238,239),-6px -4px rgb(196, 101, 25), -5px -5px rgb(237,238,239),-7px -5px rgb(196, 101, 25), -6px -6px rgb(237,238,239),-8px -6px rgb(196, 101, 25), -7px -7px rgb(237,238,239),-9px -7px rgb(196, 101, 25), -8px -8px rgb(237,238,239);');
+		});
+		$('.color2').click(function(){
+			$('body').attr('style', 'background: #1a002f;');
+			$('.left_side').attr('style', 'background: #1a002f;');
+			$('.button').attr('style', 'background: #d7385e;');
+			$('.niceline').attr('style', 'background: #d7385e; box-shadow:-2px -0px rgb(181, 45, 77), -1px -1px rgb(237,238,239),-3px -1px rgb(181, 45, 77), -2px -2px rgb(237,238,239),-4px -2px rgb(181, 45, 77), -3px -3px rgb(237,238,239),-5px -3px rgb(181, 45, 77), -4px -4px rgb(237,238,239),-6px -4px rgb(181, 45, 77), -5px -5px rgb(237,238,239),-7px -5px rgb(181, 45, 77), -6px -6px rgb(237,238,239),-8px -6px rgb(181, 45, 77), -7px -7px rgb(237,238,239),-9px -7px rgb(181, 45, 77), -8px -8px rgb(237,238,239);');
+		});
+		$('.color3').click(function(){
+			$('body').attr('style', 'background: #ff5722;');
+			$('.left_side').attr('style', 'background: #ff5722;');
+			$('.button').attr('style', 'background: #00bcd4;');
+			$('.niceline').attr('style', 'background: #00bcd4; box-shadow:-2px -0px rgb(0, 149, 168), -1px -1px rgb(237,238,239),-3px -1px rgb(0, 149, 168), -2px -2px rgb(237,238,239),-4px -2px rgb(0, 149, 168), -3px -3px rgb(237,238,239),-5px -3px rgb(0, 149, 168), -4px -4px rgb(237,238,239),-6px -4px rgb(0, 149, 168), -5px -5px rgb(237,238,239),-7px -5px rgb(0, 149, 168), -6px -6px rgb(237,238,239),-8px -6px rgb(0, 149, 168), -7px -7px rgb(237,238,239),-9px -7px rgb(0, 149, 168), -8px -8px rgb(237,238,239);');
+		});
+	</script>
